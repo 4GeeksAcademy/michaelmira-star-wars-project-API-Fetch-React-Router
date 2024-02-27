350493fdb10dec14501a2779fd5d40cfe30a27c7
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-02-27 15:44:37 +0000

    Create CaroselSlider.js

---
+++ src/js/component/CaroselSlider.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const CaroselSlider = () => {
+	return (
+		<React.Fragment>
+            <div className="container">
+                <div className="slider-wrapper">
+                    <div className="image-list">
+                        <img src="https://picsum.photos/200" alt="img-1" className="image-item"></img>
+                        <img src="https://picsum.photos/200" alt="img-2" className="image-item"></img>
+                        <img src="https://picsum.photos/200" alt="img-3" className="image-item"></img>
+                        <img src="https://picsum.photos/200" alt="img-4" className="image-item"></img>
+                        <img src="https://picsum.photos/200" alt="img-5" className="image-item"></img>
+                        <img src="https://picsum.photos/200" alt="img-6" className="image-item"></img>
+                        <img src="https://picsum.photos/200" alt="img-7" className="image-item"></img>
+                        <img src="https://picsum.photos/200" alt="img-8" className="image-item"></img>
+                        <img src="https://picsum.photos/200" alt="img-9" className="image-item"></img>
+                        <img src="https://picsum.photos/200" alt="img-10" className="image-item"></img>
+
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+	);
+};
--- src/js/component/PersonCard.js
+++ src/js/component/PersonCard.js
@@ -9,3 +9,19 @@ export const PersonCard = ({ person }) => {
 		actions.getPerson(person.uid)
 			.then(detailPerson => setDetail(detailPerson));
 	}, []);
+	return (
+        <React.Fragment>
+            {detail !== undefined ? (
+                <div className="card" style={{ width: "18rem" }}>
+                    <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{person.name}</h5>
+                        <p className="card-text">{detail.properties.eye_color}</p>
+                        <p className="card-text">{detail.properties.hair_color}</p>
+                        <Link to={"/people/" + person.uid} className="btn btn-primary">more info</Link>
+                    </div>
+                </div>
+            ) : null}
+        </React.Fragment>
+    )
+}
