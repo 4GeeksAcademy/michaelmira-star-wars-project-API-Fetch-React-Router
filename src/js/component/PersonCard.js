c961c97ac5fc285be4c16d972547fd810ce1e389
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-02-27 04:32:13 +0000

    Refactor PersonCard.js

--- src/js/component/PersonCard.js
+++ src/js/component/PersonCard.js
@@ -1,65 +1,11 @@
-const BASE_URL = "https://swapi.tech/api/";
-const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			people: [],
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			getPeople: async () => {
-				const response = await fetch(
-					BASE_URL + "people?page=1&limit=100"
-				);
-				const body = await response.json();
-				const people = body.results;
-				setStore({
-					people: people
-				});
-			},
-			getPerson: async (id) => {
-				const response = await fetch(
-					BASE_URL + "people/" + id
-				);
-				const body = await response.json();
-				const person = body.result;
-				return person;
-			},
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
-};
-
-export default getState;
+export const PersonCard = ({ person }) => {
+	const { store, actions } = useContext(Context);
+	const [ detail, setDetail ] = useState()
+	useEffect(() => {
+		actions.getPerson(person.uid)
+			.then(detailPerson => setDetail(detailPerson));
+	}, []);
