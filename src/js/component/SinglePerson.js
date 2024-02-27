07cc6aedbccab9d37a3e732189e327e9bf1e7ff4
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-02-27 02:08:40 +0000

    AppContext useState Created

--- src/js/component/SinglePerson.js
+++ src/js/component/SinglePerson.js
@@ -6,4 +6,21 @@ export const SinglePerson = (props) => {
     const params = useParams(); // {uid: "some-value"}
     const { store, actions } = useContrext(Context);
     const [detail, setDetail] = useState();
-   
+    useEffect(() => {
+        actions.getPerson(params.uid)
+        .then(detailPerson => setDetail(detailPerson));
+    }, []); 
+    return(
+        <React.Fragment>
+            {detail !== undefined ? (
+                <div className="d-flex flex column">
+                    <h2>Hello person with  id: {params.uid}</h2>
+                    <p>{detail.properties.eye_color}</p>
+                    <p>{detail.properties.hair_color}</p>
+                    <p>{detail.description}</p>
+                </div>
+            ) : null}
+        </React.Fragment>
+    )
+
+}
--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -22,7 +22,7 @@ const injectContext = PassedComponent => {
 		);
 
 		useEffect(() => {
-			
+			state.actions.getPeople()
 
 
 			/**
