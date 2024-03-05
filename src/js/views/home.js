c8bfd63da90a9371e9982e33e8d14ddca374153d
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-03-05 06:24:51 +0000

    Fix all padding on Home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -7,12 +7,12 @@ import { StarshipsSlider } from "../component/StarshipsSlider";
 
 export const Home = () => {
 	return (
-		<div  className="cardDiv text-center mt-5 d-flex flex-column w-100 align-items-center ">
-			<h2>People</h2>
+		<div  className="cardDiv text-center d-flex flex-column w-100 align-items-center ">
+			<h2 className="m-3" >People</h2>
 				<PeopleSlider />
-			<h2>Panets</h2>
+			<h2 className="m-3" >Panets</h2>
 				<PlanetsSlider />
-			<h2>Starships</h2>
+			<h2 className="m-3" >Starships</h2>
 				<StarshipsSlider />
 		</div>
 	);
