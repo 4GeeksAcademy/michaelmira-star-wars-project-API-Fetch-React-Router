8bff44c764b3a907bfb57e48495d9664372525d7
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-03-05 05:53:43 +0000

    add Starwarz logo instead of text in navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,7 +5,9 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">Michaelmira's StarWars API using Fetch, React, and Router </span>
+				<span className="navbar-brand mb-0 h1">
+					<img src="https://visualpharm.com/assets/195/Star%20Wars-595b40b85ba036ed117de471.svg" class="card-img-top" alt="Empty Image" /> 
+				</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/favorites">
