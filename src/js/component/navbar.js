8cfc650a6cccfbc915dd59e26436e5a357e3f046
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-03-05 06:01:52 +0000

    Resize starwars logo

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,7 +6,7 @@ export const Navbar = () => {
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
 				<span className="navbar-brand mb-0 h1">
-					<img src="https://visualpharm.com/assets/195/Star%20Wars-595b40b85ba036ed117de471.svg" class="card-img-top" alt="Empty Image" /> 
+					<img  src="https://visualpharm.com/assets/195/Star%20Wars-595b40b85ba036ed117de471.svg" className="card-img-top" alt="Empty Image" style={{ width: '200px', height: '100px' }} /> 
 				</span>
 			</Link>
 			<div className="ml-auto">
