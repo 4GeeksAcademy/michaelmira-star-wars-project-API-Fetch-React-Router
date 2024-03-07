04de0aefac29e212a83e4c6bf9c371db8fb2cd3a
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-03-07 20:55:49 +0000

    More Styling PeopleDetailsPage.js and change theme content

--- src/js/component/PeopleDetailsPage.js
+++ src/js/component/PeopleDetailsPage.js
@@ -15,25 +15,26 @@ export const PeopleDetailsPage = () => {
 
 	return (
         <React.Fragment>
-            <div className="card" style={{ minWidth: "18rem" }}>
-                <div className="row">
-                    <div className="col-md-6">
+            <div className="card">
+                <div className="row text-center">
+                    <div className="col-md-6 text-center">
                         <img src={BACKEND_URL+params.uid+".jpg"} style={{ maxWidth: "40rem" }}  className="card-img-top" alt="..." />
-                        <div className="col-md-6" >
-                            <div className="card-body">
-                                <h5 className="card-title">{detail?.properties.name}</h5>
-                                <p className="card-text">{detail?.properties.height}</p>
-                                <p className="card-text">{detail?.properties.mass}</p>
-                                <p className="card-text">{detail?.properties.hair_color}</p>
-                                <p className="card-text">{detail?.properties.skin_color}</p>
-                                <p className="card-text">{detail?.properties.eye_color}</p>
-                                <p className="card-text">{detail?.properties.birth_year}</p>
-                                <p className="card-text">{detail?.properties.gender}</p>
-                                <p className="card-text">{detail?.properties.description}</p>
-                                <Link to={"/"} className="btn btn-primary"> Link Home </Link>
-                            </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="card-body text-center">
+                            <h1 className="card-title">Name : {detail?.properties.name}</h1>
+                            <h3 className="card-text">Height : {detail?.properties.height}</h3>
+                            <h3 className="card-text">Weight : {detail?.properties.mass}</h3>
+                            <h3 className="card-text">Hair Color : {detail?.properties.hair_color}</h3>
+                            <h3 className="card-text">Skin Color : {detail?.properties.skin_color}</h3>
+                            <h3 className="card-text">Eye Color : {detail?.properties.eye_color}</h3>
+                            <h3 className="card-text">Birth Year : {detail?.properties.birth_year}</h3>
+                            <h3 className="card-text">Gender : {detail?.properties.gender}</h3>
+                            <h3 className="card-text">Description : {detail?.properties.description}</h3>
+                            <Link to={"/"} className="btn btn-primary"> Link Home </Link>
                         </div>
                     </div>
+                    
                 </div>       
             </div>
         </React.Fragment>
