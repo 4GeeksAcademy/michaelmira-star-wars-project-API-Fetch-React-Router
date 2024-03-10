f75e2ed931649279483a29a5782b97f1752cf1ff
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-03-10 22:21:34 +0000

    Style text Edit StarshipsDetailPAge

--- src/js/component/StarshipsDetailsPage.js
+++ src/js/component/StarshipsDetailsPage.js
@@ -22,16 +22,16 @@ export const StarshipsDetailsPage = ({ starship }) => {
                     </div>
                     <div className="col-md-6">
                         <div className="card-body text-center">
-                            <h1 className="card-title display-2 "><strong>Name : {detail?.properties.name}</strong></h1>
-                            <h3 className="card-text display-5">starship_class : {detail?.properties.starship_class}</h3>
-                            <h3 className="card-text display-5">manufacturer : {detail?.properties.manufacturer}</h3>
-                            <h3 className="card-text display-5">cost_in_credits : {detail?.properties.cost_in_credits}</h3>
-                            <h3 className="card-text display-5">length : {detail?.properties.length}</h3>
-                            <h3 className="card-text display-5">crew : {detail?.properties.crew}</h3>
-                            <h3 className="card-text display-5">passengers : {detail?.properties.passengers}</h3>
-                            <h3 className="card-text display-5">max_atmosphering_speed : {detail?.properties.max_atmosphering_speed}</h3>
-                            <h3 className="card-text display-5">hyperdrive_rating : {detail?.properties.hyperdrive_rating}</h3>
-                            <h3 className="card-text display-5">Description : {detail?.description}</h3>
+                            <h1 className="card-title display-3 "><strong>Name : {detail?.properties.name}</strong></h1>
+                            <h3 className="card-text ">Starship Class : {detail?.properties.starship_class}</h3>
+                            <h3 className="card-text ">Manufacturer : {detail?.properties.manufacturer}</h3>
+                            <h3 className="card-text ">Cost In Credits : {detail?.properties.cost_in_credits}</h3>
+                            <h3 className="card-text ">Length : {detail?.properties.length}</h3>
+                            <h3 className="card-text ">Crew : {detail?.properties.crew}</h3>
+                            <h3 className="card-text ">Passengers : {detail?.properties.passengers}</h3>
+                            <h3 className="card-text ">Max Atmosphering Speed : {detail?.properties.max_atmosphering_speed}</h3>
+                            <h3 className="card-text ">Hyperdrive Rating : {detail?.properties.hyperdrive_rating}</h3>
+                            <h3 className="card-text ">Description : {detail?.description}</h3>
                             <button className="btn btn-primary m-2" onClick={() => {
                             let checkPerson = store.favorites.find((item) => item.name === person.name && item.category === "people" )
                             if(checkPerson){
