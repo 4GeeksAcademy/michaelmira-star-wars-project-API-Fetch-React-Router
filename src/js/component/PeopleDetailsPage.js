fa7c0908ab94b60b980cfec378aa83bcd9f5d6c3
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-03-12 20:19:53 +0000

    People Details Page, Favorite function working, toggle button to and from navbar

--- src/js/component/PeopleDetailsPage.js
+++ src/js/component/PeopleDetailsPage.js
@@ -2,43 +2,46 @@ import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
 import { Link, useParams } from "react-router-dom";
 
-const BACKEND_URL = "https://starwars-visualguide.com/assets/img/characters/"
 
-export const PeopleDetailsPage = ({ person }) => {
+
+export const PeopleDetailsPage = () => {
+    const baseImgURL = "https://starwars-visualguide.com/assets/img/characters/"
     const { store, actions } = useContext(Context);
 	const [ detail, setDetail ] = useState()
     const params = useParams()
+    let person = store.people.find((item)=>item.uid==params.uid)
 	useEffect(() => {
-		actions.getPerson(params.uid, person.uid)
-			.then(detailPerson => setDetail(detailPerson));
-	}, []);
+		fetch(person.url)
+        .then(resp => resp.json())
+        .then(data => setDetail(data.result.properties))
+        
+	}, [])
 
 	return (
         <React.Fragment>
             <div className="card border border-0">
                 <div className="row text-center">
                     <div className="col-md-6 text-center">
-                        <img src={BACKEND_URL+params.uid+".jpg"} style={{ maxWidth: "40rem" }}  className="card-img-top" alt="..." />
+                        <img src={baseImgURL+params.uid+".jpg"} style={{ maxWidth: "40rem" }}  className="card-img-top" alt="..." />
                     </div>
                     <div className="col-md-6">
                         <div className="card-body text-center">
-                            <h1 className="card-title display-1 "><strong>Name : {detail?.properties.name}</strong></h1>
-                            <h1 className="card-title display-1 "><strong>Name : {person.name}</strong></h1>
-                            <h3 className="card-text display-4">Height : {detail?.properties.height}</h3>
-                            <h3 className="card-text display-4">Weight : {detail?.properties.mass}</h3>
-                            <h3 className="card-text display-4">Hair Color : {detail?.properties.hair_color}</h3>
-                            <h3 className="card-text display-4">Skin Color : {detail?.properties.skin_color}</h3>
-                            <h3 className="card-text display-4">Eye Color : {detail?.properties.eye_color}</h3>
-                            <h3 className="card-text display-4">Birth Year : {detail?.properties.birth_year}</h3>
-                            <h3 className="card-text display-4">Gender : {detail?.properties.gender}</h3>
+                            <h1 className="card-title display-1 "><strong>Name : {detail?.name}</strong></h1>
+                            <h3 className="card-text display-4">Height : {detail?.height}</h3>
+                            <h3 className="card-text display-4">Weight : {detail?.mass}</h3>
+                            <h3 className="card-text display-4">Hair Color : {detail?.hair_color}</h3>
+                            <h3 className="card-text display-4">Skin Color : {detail?.skin_color}</h3>
+                            <h3 className="card-text display-4">Eye Color : {detail?.eye_color}</h3>
+                            <h3 className="card-text display-4">Birth Year : {detail?.birth_year}</h3>
+                            <h3 className="card-text display-4">Gender : {detail?.gender}</h3>
                             <h3 className="card-text display-4">Description : {detail?.description}</h3>
-                            <button className="btn btn-primary m-2" onClick={() => {
-                            let checkPerson = store.favorites.find((item) => item.name === detail?.properties.name && item.category === "people" )
-                            if(checkPerson){
-                                actions.removeFavorite(detail?.properties.uid, "people");
-                            } else {
-                                actions.addFavorite(detail?.properties.name, detail?.properties.uid, "people");
-                            }
+                            <button className="btn btn-primary m-2" onClick={() =>{
+                                let isOnTheList = store.favorites.find((item) =>item.name==detail.name && item.category=="people")
+                                if(isOnTheList){
+                                    actions.deleteFavorite(detail.name, "people")
+                                } else {
+                                    actions.addFavorite(detail.name, person.uid, "people")
+                                }
                             }} >Favorites</button>
                             <Link to={"/"} className="btn btn-primary"> Link Home </Link>
                         </div>
