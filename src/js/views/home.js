fe48ae6e9da882a3a851dcb26b5a8d4de3f340b9
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2024-05-16 20:35:18 +0000

    Add Landing.js componet and link to home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,17 +3,20 @@ import "../../styles/home.css";
 import { PeopleSlider } from "../component/PeopleSlider";
 import { PlanetsSlider } from "../component/PlanetsSlider";
 import { StarshipsSlider } from "../component/StarshipsSlider";
+import { Landing } from "../component/Landing";
 
 
 export const Home = () => {
 	return (
-		<div  className="cardDiv text-center d-flex flex-column w-100 align-items-center ">
-			<h2 className="m-3" >People</h2>
+		<>
+			<div> <Landing /> </div><div className="cardDiv text-center d-flex flex-column w-100 align-items-center ">
+				<h2 className="m-3">People</h2>
 				<PeopleSlider />
-			<h2 className="m-3" >Panets</h2>
+				<h2 className="m-3">Panets</h2>
 				<PlanetsSlider />
-			<h2 className="m-3" >Starships</h2>
+				<h2 className="m-3">Starships</h2>
 				<StarshipsSlider />
-		</div>
+			</div>
+		</>
 	);
 };
