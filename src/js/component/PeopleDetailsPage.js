cd1d401fcd00279b00e1cf2436cda5963e6efa15
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2024-05-19 06:28:25 +0000

    make details cool with see through input

--- src/js/component/PeopleDetailsPage.js
+++ src/js/component/PeopleDetailsPage.js
@@ -25,7 +25,7 @@ export const PeopleDetailsPage = () => {
     <React.Fragment>
       <div className="card border border-0 ">
         <div className="landing" style={{ backgroundColor: "#333333" }}>
-          <div className="">
+          <div>
             <img
               className="text-light"
               style={{ maxWidth: "110rem", borderRadius: "10px" }}
@@ -33,8 +33,8 @@ export const PeopleDetailsPage = () => {
             ></img>
           </div>
           <div className="content ">
-            <div className="row text-center">
-              <div className="col-md-6 text-center  pt-4">
+            <div className="row justify-content-center">
+              <div className="col-md-6 text-center">
                 <img
                   src={baseImgURL + params.uid + ".jpg"}
                   style={{ maxWidth: "20rem", borderRadius: "10px" }}
@@ -42,57 +42,92 @@ export const PeopleDetailsPage = () => {
                   alt="..."
                 />
               </div>
-              <div className="col-md-6">
-                <div className="card-body text-center text-light ">
-                  <h1 className="card-title display-1 ">
-                    <strong>Name : {detail?.name}</strong>
-                  </h1>
-                  <h3 className="card-text display-4">
-                    Height : {detail?.height}
-                  </h3>
-                  <h3 className="card-text display-4">
-                    Weight : {detail?.mass}
-                  </h3>
-                  <h3 className="card-text display-4">
-                    Hair Color : {detail?.hair_color}
-                  </h3>
-                  <h3 className="card-text display-4">
-                    Skin Color : {detail?.skin_color}
-                  </h3>
-                  <h3 className="card-text display-4">
-                    Eye Color : {detail?.eye_color}
-                  </h3>
-                  <h3 className="card-text display-4">
-                    Birth Year : {detail?.birth_year}
-                  </h3>
-                  <h3 className="card-text display-4">
-                    Gender : {detail?.gender}
-                  </h3>
-                  <h3 className="card-text display-4">
-                    Description : {descriptionTag?.description}
-                  </h3>
-                  <button
-                    className="btn btn-primary m-2"
-                    onClick={() => {
-                      let isOnTheList = store.favorites.find(
-                        (item) =>
-                          item.name == detail.name && item.category == "people"
-                      );
-                      if (isOnTheList) {
-                        actions.deleteFavorite(detail.name, "people");
-                      } else {
-                        actions.addFavorite(detail.name, person.uid, "people");
-                      }
-                    }}
-                  >
-                    Favorites
-                  </button>
-                  <Link to={"/"} className="btn btn-primary">
-                    {" "}
-                    Link Home{" "}
-                  </Link>
+              <div className="col-md-6 pb-5 text-light">
+                <div
+                  style={{
+                    padding: "20px",
+                    borderRadius: "10px",
+                    boxShadow: "0px 0px 50px rgba(255, 255, 255, 0.2)",
+                    border: "1px solid white",
+                    backgroundColor: "rgba(0, 0, 0, 0.5)",
+                  }}
+                >
+                  <h2 style={{ textAlign: "center", marginBottom: "20px" }}>
+                    <strong> Name : {detail?.name}</strong>
+                  </h2>
+                  <div style={{ marginBottom: "20px" }}>
+                    <h5 className="card-text"> Height : {detail?.height}</h5>
+                  </div>
+                  <div style={{ marginBottom: "20px" }}>
+                    <h5 className="card-text"> Weight : {detail?.mass}</h5>
+                  </div>
+                  <div style={{ marginBottom: "20px" }}>
+                    <h5 className="card-text">
+                      {" "}
+                      Hair Color : {detail?.hair_color}
+                    </h5>
+                  </div>
+                  <div style={{ marginBottom: "20px" }}>
+                    <h5 className="card-text">
+                      {" "}
+                      Skin Color : {detail?.skin_color}
+                    </h5>
+                  </div>
+                  <div style={{ marginBottom: "20px" }}>
+                    <h5 className="card-text">
+                      {" "}
+                      Birth Year : {detail?.birth_year}
+                    </h5>
+                  </div>
+                  <div style={{ marginBottom: "20px" }}>
+                    <h5 className="card-text">
+                      {" "}
+                      Eye Color : {detail?.eye_color}
+                    </h5>
+                  </div>
+                  <div style={{ marginBottom: "20px" }}>
+                    <h5 className="card-text">Gender : {detail?.gender}</h5>
+                  </div>
+                  <div style={{ textAlign: "center" }}>
+                    <h5 className="card-text">
+                      {" "}
+                      Description : {descriptionTag?.description}
+                    </h5>
+                  </div>
+                  <div>
+                    <button
+                      className="btn btn-primary m-2"
+                      onClick={() => {
+                        let isOnTheList = store.favorites.find(
+                          (item) =>
+                            item.name == detail.name &&
+                            item.category == "people"
+                        );
+                        if (isOnTheList) {
+                          actions.deleteFavorite(detail.name, "people");
+                        } else {
+                          actions.addFavorite(
+                            detail.name,
+                            person.uid,
+                            "people"
+                          );
+                        }
+                      }}
+                    >
+                      Favorites
+                    </button>
+                    <Link to={"/"} className="btn btn-primary">
+                      {" "}
+                      Link Home{" "}
+                    </Link>
+                  </div>
                 </div>
               </div>
+            </div>
+          </div>
+          <div className="row text-center">
+            <div className="col-md-6">
+              <div className="card-body text-center text-light "></div>
             </div>
           </div>
         </div>
